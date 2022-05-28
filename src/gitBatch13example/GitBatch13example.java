067de17966f31c1692f0043e32cc271a97fdf82e
Ogai <Ogai.ahmadi@gmail.com>
2022-05-27 21:22:04 -0700

    initial commit

--- src/gitBatch13example/GitBatch13example.java
+++ src/gitBatch13example/GitBatch13example.java
@@ -6,6 +6,7 @@ public class GitBatch13example {
 		// TODO Auto-generated method stub
 		System.out.println("This is second class in git");
 		System.out.println("This is second class in git");
+		System.out.println("lets make some changes");
 	}
 	
 
